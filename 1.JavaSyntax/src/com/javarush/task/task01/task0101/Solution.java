76388b236ddfe1d0e6852fe6e83ac2fe25508162
NAleksanin <na9584472@gmail.com>
2025-03-26 19:25:33 +0300

    добвил решение для задачи task0101

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0101;
+
+/* 
+Я думаю, быть программистом круто
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s  = "Я думаю, быть программистом круто";
+        System.out.println(s);
+        //напишите тут ваш код
+    }
+}
