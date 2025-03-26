04649d1b2750432816dac913c9856e0f5dedb9f1
NAleksanin <na9584472@gmail.com>
2025-03-26 19:46:05 +0300

    добвил решение для задачи task0105

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+Объявляем переменные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "nikola";
+        int age = 37;
+        String city = "moscva";
+        //напишите тут ваш код
+    }
+}
