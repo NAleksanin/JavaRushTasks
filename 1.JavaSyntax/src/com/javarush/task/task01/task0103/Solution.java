4571c52b38d0c225726fbb978fab0ee44708226d
NAleksanin <na9584472@gmail.com>
2025-03-26 19:42:46 +0300

    добвил решение для задачи task0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3126;
+        System.out.println(a - 8);
+        //напишите тут ваш код
+    }
+}
