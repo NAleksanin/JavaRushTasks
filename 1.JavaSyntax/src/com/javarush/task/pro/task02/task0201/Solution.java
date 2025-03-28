3931646e855c86944b5f27eeb5818dc9ccb61650
NAleksanin <na9584472@gmail.com>
2025-03-28 19:34:48 +0300

    добвил решение для задачи task0142

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 3;
+        int numericValue = 6;
+        int size = 4;
+        int number = 7;
+        System.out.println("«Я по объявлению…»");
+        //напишите тут ваш код
+
+    }
+}
