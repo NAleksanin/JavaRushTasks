1814f73c0a1f25583bbfbf8f690520267dc698c3
NAleksanin <na9584472@gmail.com>
2025-03-30 20:14:04 +0300

    добвил решение для задачи task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "ljv";
+        String phrase = "fhd";
+        String line = "asdsfgdg";
+        String text = "xgdfxcg";
+
+        //напишите тут ваш код
+
+    }
+}
